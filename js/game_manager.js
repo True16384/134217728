1772510b02fe666687c7455589490d84d01ce780
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-11-20 14:14:42 -0600

    Increase difficulty

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.75 ? 2 : 4;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -139,8 +139,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 65536) self.won = true;
+          // The mighty 134217728 tile
+          if (merged.value === 134217728) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
